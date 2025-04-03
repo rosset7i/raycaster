1379ad96b48c4393cc26f5dc49036ac900448470
Matheus Henrique Custodio Rossetti <matheus.rossetti_ext@aldo.com.br>
2025-04-03 12:05:20 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use glium::{
+    backend::glutin::SimpleWindowBuilder,
+    winit::{
+        application::ApplicationHandler,
+        event::WindowEvent,
+        event_loop::{ActiveEventLoop, EventLoop},
+        window::WindowId,
+    },
+};
+
+const WORLD_MAP: [[u8; 16]; 16] = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+#[derive(Default)]
+struct Application {}
+
+impl ApplicationHandler for Application {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+    }
+}
+
+fn main() {
+    let position_x: f32 = 8.0;
+    let position_y: f32 = 8.0;
+
+    let direction_x: f32 = -1.0;
+    let direction_y: f32 = 0.0;
+
+    let plane_x: f32 = 0.0;
+    let plane_y: f32 = 0.66;
+
+    let time: f32 = 0.0;
+    let old_time: f32 = 0.0;
+
+    let event_loop = EventLoop::builder().build().unwrap();
+    let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
+
+    let mut app = Application::default();
+    event_loop.run_app(&mut app).unwrap();
+}
